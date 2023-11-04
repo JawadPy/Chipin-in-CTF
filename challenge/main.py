5cdd69bccd89b8c96edcc2857cf627ec35cd3676
Jawad <98477272+JawadPy@users.noreply.github.com>
2023-11-04 12:28:30 +0300

    Update main.py

--- challenge/main.py
+++ challenge/main.py
@@ -2,13 +2,10 @@ from flask import Flask, request, render_template as rt
 from socket import gethostname
 from yaml import load, dump
 import os
+import sys
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
-        
-def generate_flag():
-    with open(r'web\flag.txt', 'w') as f:
-        f.write("NC{{{}}}".format(os.urandom(16).hex()))
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -22,6 +19,5 @@ def index():
     return rt('index.html')
 
 if __name__ == '__main__':
-    generate_flag()
     if 'liveconsole' not in gethostname():
-        app.run(debug=False, host='0.0.0.0', port=80)
+        app.run(debug=False, host='0.0.0.0', port=int(sys.argv[1]))
